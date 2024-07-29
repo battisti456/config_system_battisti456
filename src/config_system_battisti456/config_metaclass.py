8fbc30689a4a2d52dc575ee4a3fa5c44cc48e18a
battisti456 <battisti456@gmail.com>
2024-07-29 12:15:15 -0700

    fixed override reversing

--- src/config_system_battisti456/config_metaclass.py
+++ src/config_system_battisti456/config_metaclass.py
@@ -21,7 +21,7 @@ class Config_Metaclass(type):
     def __getattribute__(cls, name: str) -> Any:
         if name in ('_overrides','_name'):
             return type.__getattribute__(cls,name)
-        for config_override in cls._overrides.__reversed__():
+        for config_override in reversed(cls._overrides):
             if config_override.defines_property(cls.__name, name):
                 return config_override.get_property(cls.__name, name)
         return super().__getattribute__(name)
